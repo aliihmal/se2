8cda2ffa823bda96dcf1acecf77954a02ec0ad8f
Ali hamza <alihamza637376@gmail.com>
2026-02-11 09:53:25 +0200

    Third commit

--- src/app_cleaned.ts
+++ src/app_cleaned.ts
@@ -4,7 +4,7 @@ export interface Order{
     item:string;
     price:number;
     id:number;
-}// first we create the order interface so we can use it dsf
+}// first we create the order interface so we can use it dsfd
 
 export class orderManagement{
     private orders:Order[] = [] ;
